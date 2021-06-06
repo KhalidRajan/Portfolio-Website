852f2d0999ff697a49a507cd7cde33d98389d9f6
Khalid Rajan <khalidrajan14@gmail.com>
2021-06-05 21:02:58 -0600

    Bugs fixes

--- src/navigation.js
+++ src/navigation.js
@@ -1,19 +1,20 @@
-import React from "react"
-import "./navigation.css"
+import React from "react";
+import {BrowserRouter, Link } from "react-router-dom";
+import "./navigation.css";
 
 function Navigation(){
     return(
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="/"><h3 className="logo">Khalid Rajan</h3></a>
+                <Link class="navbar-brand" to={"/"}><h3 className="logo">Khalid Rajan</h3></Link>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link" href="/about">About Me</a>
+                            <Link class="nav-link" to={"/about"}>About Me</Link>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/projects">Projects</a>
+                            <Link class="nav-link" to={"/projects"}>Projects</Link>
                         </li>
                     </ul>
 
